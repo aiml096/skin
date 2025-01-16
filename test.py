ff8294720e833454dbaa74220d8d79fbdcb4569e
aiml096 <160322462+aiml096@users.noreply.github.com>
2025-01-16 11:12:32 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,79 @@
+import pickle
+import cv2
+import mediapipe as mp
+import numpy as np
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {0: 'Hello', 1: 'Nammeste', 2: 'love', 3: 'One', 4: 'E', 5: 'Super', 6: 'I am', 7: 'Krishna', 8: 'washroom', 9: 'K',
+               10: 'water', 11: 'K', 12: 'knock', 13: 'no', 14: 'P', 15: 'Q', 16: 'Happy', 17: 'S',
+               18: 'smile', 19: '2 minutes', 20: 'congratulations', 21: 'W', 22: 'hang', 23: 'call', 24 : 'PLEASE', 25 : 'thanyou',
+               }
+
+while True:
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+        # Assuming the model expects 42 features
+        prediction = model.predict([np.asarray(data_aux[:42])])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+        
+
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
